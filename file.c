b6b1537563d93ceb4d12605cf56bcade25443f93
antonsamy20 <antonsamy2004@gmail.con>
2025-07-23 03:19:07 +0300

    update

--- file.c
+++ file.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>  
 
+void loadFromCSV(){
+    printf("Loading data from CSV...\n");
+    FILE *file = fopen("books.csv","r");
+    if (file == NULL){
+        printf("Error opening file.\n");
+        exit(1);
+    }
+
+}
+
+void saveToCSV(){
+    printf("Saving data to CSV...\n");
+}
--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<admin.h>
+#include<user.h>
+#include<file.h>
 
+void display_menu(){
+    printf("Welcome to the file management system!\n");
+    printf("1. Admin login\n");
+    printf("2. User login\n");
+    printf("3. Exit\n");
+    printf("please select an option: ");
+}
+
+int main(){
+    int choice;
+    loadFromCSV();
+    while (true){
+        display_menu();
+        scanf("%d", &choice);
+        switch (choice){
+            case 1:
+                admin_login();
+                break;
+            case 2:
+                user_login();
+                break;
+            case 3:
+                saveToCSV();
+                printf("Exiting the system. Goodbye!\n");
+                exit(0);
+            default:
+                printf("Invalid choice, please try again.\n");
+        }
+    }
+    return 0;
+}
