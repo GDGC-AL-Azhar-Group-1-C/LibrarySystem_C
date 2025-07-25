01228e21efe8f3e0109de03c58a6e77c40f70503
rahma28317 <ra2813717@gmail.com>
2025-07-25 22:35:44 +0300

    Updated viewAvailableBooks and returnBook to use array

--- user.c
+++ user.c
@@ -1,10 +1,10 @@
 void viewAvailableBooks()
  {
     Book books[100];
-    int count = 0;
-    readBooks(books, &count);
+    int count =0;
 
 printf("Available books:\n");
+    int found = 0;
     for (int i = 0; i < count; i++) 
     {
     if (books[i].isBorrowed == 0) 
@@ -12,32 +12,35 @@ printf("Available books:\n");
 printf("ID: %d | Title: %s | Author: %s | Year: %d\n",
      books[i].id, books[i].title,
      books[i].author, books[i].year);
-       
+ found = 1;      
     }
+    }
+      if (!found) {
+      printf("No available books at the moment.\n");
     }
     } 
 
 
 void returnBook() {
     Book books[100];
-    int count, id, found = 0;
-    
-    readBooks(books, &count);  
+    int count, id, found = 0;  
 
 printf("Please enter the book ID to return:\n");
 scanf("%d", &id);
 
     for (int i = 0; i < count; i++) {
     if (books[i].id == id && books[i].isBorrowed == 1) {
-            books[i].isBorrowed = 0;  
-            found = 1;
-            break;
-}
-}
-    if (found) {
-        writeBooks(books, count); 
-        printf("Book returned successfully.\n");
+            books[i].isBorrowed = 0;     
+    printf("Book returned successfully.\n");
+    found = 1;
 }   else {
         printf("This book is not currently borrowed.\n");
+    found =1;    
 }
+    break;
+} 
+}
+    if (!found)  {
+                  printf("Book with ID %d not found.\n",id);
+
 }
